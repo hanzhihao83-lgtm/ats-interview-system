9e73e3628ba921e7abe5a24b23498ceb5a3fd626
韩志号 <hanzhihao@hanzhihaodeMacBook-Air.local>
2026-08-02 13:12:22 +0800

    sync Vercel asset path config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ base: process.env.CI ? "/ats-interview-system/" : "/", plugins: [react()], server: { proxy: { "/api": { target: "http://localhost:3001", changeOrigin: true } } } });
+export default defineConfig({ base: process.env.GITLAB_CI ? "/ats-interview-system/" : "/", plugins: [react()], server: { proxy: { "/api": { target: "http://localhost:3001", changeOrigin: true } } } });
